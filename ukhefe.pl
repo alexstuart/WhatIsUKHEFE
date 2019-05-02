f2c47518f76ff60285a6f9e6f75ee0d8d6b83703
Alex Stuart <alex.stuart@jisc.ac.uk>
2019-05-02 21:35:26 +0100

    Add ability to allow or block individual entities

--- ukhefe.pl
+++ ukhefe.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl -w
 #
-# Given a metadata file, this script outputs an unsigned metadata aggregate
-# that roughly corresponds to UK HE and FE IdPs
+# Given a metadata aggreagte, this script outputs an unsigned metadata aggregate
+# that roughly corresponds to UK HE and FE IdPs in it.
 #
 # Author: Alex Stuart, alex.stuart@jisc.ac.uk
 #
@@ -22,6 +22,7 @@
 
 use XML::LibXML;
 use Getopt::Long;
+$DEBUG = 0;
 
 sub usage {
         my $message = shift(@_);
@@ -29,27 +30,49 @@ sub usage {
 
         print <<EOF;
 
-        usage: $0 [-h] <file>
+        usage: $0 [-h] [-b <entityID>] [-a <entityID>] <file>
 
-        -h - print this help text and exit
-
-        Given an EntityDescriptor or EntitiesDescriptor, output an EntitiesDescriptor
-        which roughly contains IdPs from UK HE and FE.
+        Given a SAML metadata aggregate, output an unsigned EntitiesDescriptor
+        which contains IdPs
 
         - registered by the UK federation
         - contain a scope that ends .ac.uk
-        - visible IdP
+        - are visible
+
+        Typically you would input the UK federation metadata aggregate to this script.
+        This gives a first approximation to the community of UK HE and FE providers.
+        Then add or remove entities to build the community of IdPs that you require for
+        your specific application. Explicitly added entities are added to the output,
+        even if they are explicitly blocked or not selected by the algorithm.
+
+	-b <entityID>	- entityID will be removed from output.
+			  More than one -b <entityID> can be given.
+        -a <entityID>	- entityID that is explicitly allowed.
+			  More than one -a <entityID> can be given.
+
+        -h 		- print this help text and exit
+        -d		- print debug information. NB: this is to STDOUT as is the XML so beware!
 
 EOF
 }
 
 my $help;
-GetOptions (    "help" => \$help
+my @blocked;
+my @allowed;
+my $debug;
+GetOptions (    "help" => \$help,
+		"debug" => \$debug,
+		"blocked=s@" => \@blocked,
+		"allowed=s@" => \@allowed
            );
 
 if ( $help ) {
         usage;
         exit 0;
+}
+
+if ( $debug ) {
+	$DEBUG = 1;
 }
 
 if ( ! $ARGV[0] ) {
@@ -64,12 +87,47 @@ if ( ! -r $xmlfile ) {
         exit 1;
 }
 
+#
+# Blocklists and explicit additions
+#
+
+$DEBUG && print "Allow list: " . join(' ', @allowed) . "\n";
+$DEBUG && print "Blocklist: " . join(' ', @blocked) . "\n";
+
+#
+# Aggregate header
+#
+binmode(STDOUT, ":utf8");
+print STDOUT <<EOF;
+<?xml version="1.0" encoding="UTF-8"?>
+<EntitiesDescriptor xmlns="urn:oasis:names:tc:SAML:2.0:metadata">
+EOF
+
+#
+# Ingest XML file
+#
 my @nodes;
 my $dom = XML::LibXML->load_xml( location => $xmlfile);
 my $xpc = XML::LibXML::XPathContext->new( $dom );
 $xpc->registerNs( 'md', 'urn:oasis:names:tc:SAML:2.0:metadata' );
 $xpc->registerNs( 'mdrpi', 'urn:oasis:names:tc:SAML:metadata:rpi' );
 $xpc->registerNs( 'shibmd', 'urn:mace:shibboleth:metadata:1.0' );
+
+#
+# Allowed entities are printed first
+#
+for $allowed (@allowed) {
+        $DEBUG && print "Finding $allowed\n";
+        @nodes = $xpc->findnodes( '//md:EntityDescriptor[./@entityID="'.$allowed.'"]');
+        for $node (@nodes) {
+                $newnode = $node->cloneNode( 1 );
+                print $newnode;
+        }
+}
+
+#
+# Now the algorithmically-determined entities
+#
 
 # From the XML::LibXML::XPathContext documentation
 $xpc->registerFunction('grep_nodes', \&grep_nodes);
@@ -82,9 +140,29 @@ $xpc->registerFunction('grep_nodes', \&grep_nodes);
 		');
 
 foreach $node (@nodes) {
-	@attributelist = $node->attributes();
-	print join(';', @attributelist) . "\n";
+# Removing any in the blocklist
+	my @attributelist = $node->attributes();
+	$blocked = 0;
+	foreach $attribute ( $node->attributes() ) {
+		foreach $block ( @blocked ) {
+			if ( $attribute =~ m/^\s*entityID="$block"\s*$/ ) { $blocked = 1; }
+		}
+	}
+	next if $blocked == 1;
+
+# Uses cloneNode to get all the namespace prefix definitions
+	$newnode = $node->cloneNode( 1 );
+	print $newnode;
 }
+
+#
+# Aggregate footer
+#
+print STDOUT "\n</EntitiesDescriptor>\n";
+
+#
+# Subroutines ----------------------------------------------------------------------
+#
 
 # From the XML::LibXML::XPathContext documentation
 sub grep_nodes {
